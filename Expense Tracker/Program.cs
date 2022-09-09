049100ba965f4feb938347e78372edfbbcf4c43d
Darek <dariuszgrados@gmail.com>
2022-09-09 10:29:56 +0200

    css on create category

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -16,6 +16,9 @@ namespace Expense_Tracker
             builder.Services.AddDbContext<ApplicationDbContext>(options => 
             options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VVhiQlFaclxJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkxjUH9ddHNUT2RZVkE=");
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
