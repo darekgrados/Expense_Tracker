83df0bf7d70b70973776ac5d61263567817e3e1c
Darek <dariuszgrados@gmail.com>
2022-09-11 17:39:21 +0200

    SideBar config

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -38,7 +38,7 @@ namespace Expense_Tracker
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
             app.Run();
         }
